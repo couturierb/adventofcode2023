bb73a191fa1b1b2f20ba0bed32999f6dd4efb5bf
Boris Couturier <boris.couturier@worldline.com>
2023-12-14 14:48:05 +0100

    Day 8 - second part

--- src/main/kotlin/days/Day8.kt
+++ src/main/kotlin/days/Day8.kt
@@ -6,7 +6,6 @@ class Day8 : Day(8) {
     lateinit var directions: CharArray
 
     override fun partOne(): Any {
-        return ""
         directions = inputList.get(0).toCharArray()
         inputList
             .filterIndexed { index, s -> index > 0 }
@@ -42,21 +41,18 @@ class Day8 : Day(8) {
         var count = 0
         var indexes = network.keys.filter { it.endsWith("A") }.toList().toMutableList()
 
-        var multiples = mutableListOf(13200, 22410, 18726, 18112, 16270, 20568)
-        while(true) {
-            multiples.min()
+        while (indexes.any { !it.endsWith("Z") }) {
+            indexes.forEachIndexed { i, s ->
+                indexes[i] = if (directions[count%directions.size] == 'L') network.get(s)!!.first else network.get(s)!!.second
+                if (indexes[i].endsWith("Z")) {
+                    println("${i}: ${count+1}")
+                }
+            }
+            count++
         }
 
-//        while (indexes.any { !it.endsWith("Z") }) {
-//            indexes.forEachIndexed { i, s ->
-//                indexes[i] = if (directions[count%directions.size] == 'L') network.get(s)!!.first else network.get(s)!!.second
-//                if (indexes[i].endsWith("Z")) {
-//                    println("${i}: ${count}")
-//                }
-//            }
-//            count++
-//        }
-
+        //  Calculer le PPCM de : 13201, 22411, 18727, 18113, 16271, 20569
+        // https://www.wolframalpha.com/input?i=what+is+the+lcm+of+13201%2C+22411%2C+18727%2C+18113%2C+16271%2C+20569
         return count
     }
 
